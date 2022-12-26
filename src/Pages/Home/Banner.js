05e30c67512faeacc76904dec4c19fb7acde8bf9
arnob-das <arnob.cse.20210104148@aust.edu>
2022-12-26 06:44:19 +0600

    hero section done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen lg:px-12">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="Chair Background" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                    <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary bg-gradient-to-primary">Get Started</button>
                 </div>
             </div>
         </div>
